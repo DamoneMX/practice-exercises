c9b1fd0b77eafed067b8361c8d4abea6bc766358
Arie R <arie@dadabureau.com>
2014-02-13 10:26:31 -0800

    adding first exercises

---
+++ DeleteDuplicates.java
@@ -0,0 +1,33 @@
+/**
+ * Problem: 
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * 
+ * Definition for singly-linked list. 
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while(current != null){
+
+            ListNode check = current.next;
+	    //Spent time on check.next nullcheck, if needed don't hesitate to add
+            while(check != null && check.next != null && check.next.val == current.val) 
+                check = check.next;
+            
+            if(check != null && current.val == check.val)
+               current.next = check.next;
+            
+            current = current.next;
+        }
+        
+        return head;
+    }
+}
