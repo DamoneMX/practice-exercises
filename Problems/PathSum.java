1d7f37535aa754c52a96e9c5b96daa36fe820edf
Arie R <ArieRadilla@gmail.com>
2014-04-27 01:36:27 -0700

    follow up to path sum

--- Problems/PathSum.java
+++ Problems/PathSum.java
@@ -2,7 +2,9 @@
  * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that 
  * adding up all the values along the path equals the given sum. 
  *
- *
+ * Follow Up:  Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum. 
+ * Method : pathSum
+ * 
  * Solution:
  * Medium difficulty problem.
  * Good Problem to review BST problems.
@@ -33,4 +35,42 @@ public class Solution{
         
         return false;
     }
+
+
+    public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        getPathSum(root, new ArrayList<Integer>(),  result, sum);
+        return result;
+    }
+    
+    public void getPathSum(TreeNode node, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> result, int sum){
+        if(node.left == null && node.right == null){
+            int total = 0;
+            for(Integer number : path){
+                total += number;
+            }
+            
+            total += node.val;
+            if(total == sum){
+                path.add(node.val);
+                result.add(path);
+            }
+            
+            return;
+        }
+        
+        
+        if(node.left != null){
+            ArrayList<Integer> copy = new ArrayList<Integer>();
+            copy.addAll(path);
+            getPathSum(node.right, copy,result, sum);
+        }
+            
+        if(node.right != null){
+            ArrayList<Integer> copy = new ArrayList<Integer>();
+            copy.addAll(path);
+            getPathSum(node.right, copy,result, sum);
+        } 
+    }
+
 }
