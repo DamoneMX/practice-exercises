523d09595dd330a1b4d30e0b451f24e1e4230ca5
Arie R <ArieRadilla@gmail.com>
2014-04-29 20:24:23 -0700

    solution

---
+++ Problems/BSTMinDepth.java
@@ -0,0 +1,38 @@
+/**
+ * Given a binary tree, find its minimum depth.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BSTMinDepth {
+    public int minDepth(TreeNode root) {
+		if(root == null)
+			return 0;
+			
+		return minDepth(root, 1);
+    }
+	
+	public int minDepth(TreeNode node, int depth){
+		if(node.left == null && node.right == null)
+			return depth;
+		
+		int n1 = depth; 
+		int n2 = depth;
+		if(node.left != null)
+			n1 = minDepth(node.left, depth + 1);
+		
+		if(node.right != null)
+			n2 = minDepth(node.right, depth + 1);
+		
+		if(n1 == depth)
+			return n2;
+		
+		if(n2 == depth)
+			return n1;
+		
+		return Math.min(n1, n2);
+	}
+}
