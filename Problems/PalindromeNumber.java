20051c702c75b4018a86f7819ff8abe6c66b0bd2
Arie R <ArieRadilla@gmail.com>
2014-05-13 22:10:31 -0700

    addin problem

---
+++ Problems/PalindromeNumber.java
@@ -0,0 +1,39 @@
+/**
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ */
+public class PalindromeNumber{
+	public boolean isPalindrome(int x) {
+        if(x < 0)
+			return false;
+		
+		int ceros = 1;
+		while((x / ceros) >= 10){
+			ceros *= 10;
+		}
+		
+		//if(ceros == 1)
+			//return true;
+		
+		int lowest = 10;
+		
+		while(lowest <= ceros){
+			int high = x / ceros;
+			int low = x % lowest;
+			low = low / (lowest / 10);
+			if(high != low)
+				return false;
+			
+			x = x % ceros;
+			ceros = ceros / 10;
+			
+			lowest = lowest * 10;
+			
+		}
+
+		return true;
+    }
+
+    public static void main(String[] args){	
+		System.out.println(isPalindrome(2147483647));
+	}
+}
