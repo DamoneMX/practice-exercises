75b80d5e247a42b89857e37ba838f6975f89d38a
Arie Radilla <arieradilla@gmail.com>
2014-10-11 14:30:16 -0700

    Follow up to old problem

--- Problems/LinkedListHasCycle.java
+++ Problems/LinkedListHasCycle.java
@@ -21,4 +21,61 @@ public class LinkedListHasCycle {
                 return true;
         }
     }
+
+    //Follow up: 
+    //Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+    public static ListNode detectCycle(ListNode head) {
+        if(head == null)
+            return null;
+        
+        //find out if there is a loop
+        ListNode seeker = head;
+        ListNode runner = head;
+        ListNode loopNode = null;
+        
+        while(runner.next != null && runner.next.next != null){
+            seeker = seeker.next;
+            runner = runner.next.next;
+            if(seeker == runner){
+                loopNode = seeker;
+                break;
+            }
+        }
+        
+        if(loopNode == null)
+            return null;
+
+        //count how many nodes are part of the loop
+        seeker = loopNode.next;
+        int count = 2;
+        while(true) {
+            seeker = seeker.next;
+            if(seeker == loopNode){ 
+                break;
+            } else {
+                count++;
+            }
+        }
+    
+        //start two pointers, one from head, the other one at kth node from head
+        //move them at same pace, they'll meet at the place where the loop starts
+        
+        //place seeker at kth steps from start
+        seeker = head;
+        for(int i = 1; i <= count; i++){
+            seeker = seeker.next;
+        }
+        
+        
+        runner = head;
+        while(true){
+            if(seeker == runner)
+                return seeker;
+            else {
+                seeker = seeker.next;
+                runner = runner.next;
+            }
+        }
+    }
+
 }
